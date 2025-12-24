5a01cfc56cad911331e05a20801b4083662f17de
𝐌 𝐔 𝐑 𝚨 𝐋 𝐈 𓂃ִֶָ⋆.˚ 🔮 <148250637+Itz-Murali@users.noreply.github.com>
2025-12-24 17:03:56 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "@/core/ui/toaster";
+import { Toaster as Sonner } from "@/core/ui/sonner";
+import { TooltipProvider } from "@/core/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import Home from "./views/Home";
+import AllProjects from "./views/AllProjects";
+import NotFound from "./views/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<AllProjects />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </HelmetProvider>
+);
+
+export default App;
